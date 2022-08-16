d0e4d723c8f2fabc85f72fb98f2fc6b57e716890
jwaine44 <jwaine44@gmail.com>
2022-08-16 12:05:53 -0400

    Initialize Repo

---
+++ MinHeap-ExtractSmallest.js
@@ -0,0 +1,123 @@
+/**
+ * Class to represent a MinHeap which is a Priority Queue optimized for fast
+ * retrieval of the minimum value. It is implemented using an array but it is
+ * best visualized as a tree structure where each 'node' has left and right
+ * children except the 'node' may only have a left child.
+ * - parent is located at: Math.floor(i / 2);
+ * - left child is located at: i * 2
+ * - right child is located at: i * 2 + 1
+ */
+class MinHeap {
+constructor() {
+    /**
+     * 0th index not used, so null is a placeholder. Not using 0th index makes
+     * calculating the left and right children's index cleaner.
+     * This also effectively makes our array start at index 1.
+     */
+    this.heap = [null];
+}
+
+/**
+ * Extracts the min num from the heap and then re-orders the heap to
+ * maintain order so the next min is ready to be extracted.
+ * 1. Save the first node to a temp var.
+ * 2. Pop last node off and set idx1 equal to the popped value.
+ * 3. Iteratively swap the old last node that is now at idx1 with it's
+ *    smallest child IF the smallest child is smaller than it.
+ * - Time: O(log n) logarithmic due to shiftDown.
+ * - Space: O(1) constant.
+ * @returns {?number} The min number or null if empty.
+ */
+extract(){
+    let current = this.heap.length - 1;
+    this.heap[1] = this.heap[current];
+    this.heap.pop();
+    current = 1;
+    while(this.heap[current] > this.heap[(current * 2)] || this.heap[current] > this.heap[(current * 2) + 1]){
+        if(this.heap[(current * 2)] < this.heap[(current * 2) + 1] && this.heap[(current * 2)] != null){
+            [this.heap[(current * 2)], this.heap[current]] = [this.heap[current], this.heap[(current * 2)]];
+            current = (current * 2)
+        }
+        if(this.heap[(current * 2)] < this.heap[(current * 2) + 1] && this.heap[(current * 2) + 1] != null){
+            [this.heap[(current * 2) + 1], this.heap[current]] = [this.heap[current], this.heap[(current * 2) + 1]];
+                current = (current * 2) + 1;
+        }
+        else {
+            break;
+        }
+    }
+    return this.heap[1];
+}
+
+top() {
+    return this.heap.length > 1 ? this.heap[1] : null;
+}
+
+/**
+ * Inserts a new number into the heap and reorders heap to maintain order.
+ * 1. Push new num to back.
+ * 2. Iteratively swap the new num with it's parent while it is smaller than
+ *    it's parent.
+ * - Time: O(log n) logarithmic due to shiftUp.
+ * - Space: O(1) constant.
+ * @param {number} num The num to add.
+ */
+insert(num) {
+    this.heap.push(num);
+    this.shiftUp();
+    // .push on array returns the new length
+    return this.heap.length;
+}
+
+// AKA: siftUp, heapifyUp, bubbleUp to restore order after insert
+shiftUp() {
+    let idxOfNodeToShiftUp = this.heap.length - 1;
+
+    while (idxOfNodeToShiftUp > 1) {
+        const idxOfParent = this.idxOfParent(idxOfNodeToShiftUp);
+
+        const isParentSmallerOrEqual =
+        this.heap[idxOfParent] <= this.heap[idxOfNodeToShiftUp];
+
+        // Parent is supposed to be smaller so ordering is complete.
+        if (isParentSmallerOrEqual) {
+        break;
+        }
+
+        this.swap(idxOfNodeToShiftUp, idxOfParent);
+        // after swapping the node is at idxOfParent now.
+        idxOfNodeToShiftUp = idxOfParent;
+    }
+}
+
+/**
+ * Logs the tree horizontally with the root on the left and the index in
+ * parenthesis using reverse inorder traversal.
+ */
+printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
+    if (parentIdx > this.heap.length - 1) {
+    return;
+    }
+
+    spaceCnt += spaceIncr;
+    this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
+
+    console.log(
+    " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+        `${this.heap[parentIdx]} (${parentIdx})`
+    );
+
+    this.printHorizontalTree(parentIdx * 2, spaceCnt);
+    }
+}
+
+let newHeap = new MinHeap();
+newHeap.insert(12);
+newHeap.insert(3);
+newHeap.insert(4);
+newHeap.insert(9);
+newHeap.insert(69);
+newHeap.insert(42);
+newHeap.insert(420);
+
+console.log(newHeap.extract());
